0f43764f9e671ad778f9d0709403c7b3c4a26009
Aleksey Voronoy <voronoy@onix-systems.com>
2020-11-04 15:57:39 +0200

    fix travis-ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -22,7 +22,6 @@ unset($findRoot);
 chdir($root);
 if (file_exists($root . '/config/bootstrap.php')) {
     require $root . '/config/bootstrap.php';
-
     return;
 }
 
@@ -31,14 +30,16 @@ if (is_readable(__DIR__ . '/env.php')) {
     require_once 'env.php';
 }
 \Cake\Core\Configure::write('App.namespace', 'Voronoy\PgUtils\Test\TestApp');
-
 $dbConfig = [
     'className' => Connection::class,
     'driver' => Postgres::class,
     'host' => getenv('db_host'),
-    'username' => getenv('db_user'),
-    'password' => getenv('db_pass'),
-    'database' => getenv('db_name'),
+    'username' => getenv('db_user') ?: null,
+    'password' => getenv('db_pass') ?: null,
+    'database' => getenv('db_name') ?: null,
+    'timezone' => 'UTC',
+    'quoteIdentifiers' => false,
+    'cacheMetadata' => false,
 ];
 ConnectionManager::drop('test');
 ConnectionManager::setConfig('test', $dbConfig);
